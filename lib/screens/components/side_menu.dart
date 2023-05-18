c49e4b254047e2572bd664c8ad0a1e7bd9bd73c5
isha jain <isha30435@gmail.com>
2023-05-18 23:46:04 +0530

    new file for side bar added

--- lib/screens/components/side_menu.dart
+++ lib/screens/components/side_menu.dart
@@ -12,25 +12,25 @@ class SideMenu extends StatelessWidget {
     return Drawer(
         child: Column(
       children: [
-        MyInformation(),
+        const MyInformation(),
         Expanded(
           child: SingleChildScrollView(
-            padding: EdgeInsets.all(defaultPadding),
+            padding: const EdgeInsets.all(defaultPadding),
             child: Column(
               children: [
-                AddressInfo(
+                const AddressInfo(
                   title: "Address",
                   text: "India",
                 ),
-                AddressInfo(
+                const AddressInfo(
                   title: "City",
                   text: "Jabalpur",
                 ),
-                AddressInfo(
+                const AddressInfo(
                   title: "Age",
                   text: "22",
                 ),
-                Divider(),
+                const Divider(),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: defaultPadding),
                   child: Text(
@@ -38,48 +38,85 @@ class SideMenu extends StatelessWidget {
                     style: Theme.of(context).textTheme.subtitle2,
                   ),
                 ),
-                Column(
+                const Row(
                   children: [
-                    AspectRatio(
-                      aspectRatio: 1,
-                      child: TweenAnimationBuilder(
-                          tween: Tween<double>(begin: 0, end: 1),
-                          duration: defaultDuration,
-                          builder: (context, double value, child) => Stack(
-                                fit: StackFit.expand,
-                                children: [
-                                  CircularProgressIndicator(
-                                    value: value,
-                                    color: primaryColor,
-                                    backgroundColor: darkColor,
-                                  ),
-                                  Center(
-                                    child: Text(
-                                      (value = 100).toInt().toString() + "%",
-                                      style:
-                                          Theme.of(context).textTheme.subtitle1,
-                                    ),
-                                  )
-                                ],
-                              )),
+                    Expanded(
+                      child: AnimatedCircularProgressIndicator(
+                        percentage: 0.8,
+                        label: "Flutter",
+                      ),
                     ),
-                    SizedBox(
-                      height: defaultPadding / 2,
+                    SizedBox(width: defaultPadding),
+                    Expanded(
+                      child: AnimatedCircularProgressIndicator(
+                        percentage: 0.72,
+                        label: "c++",
+                      ),
                     ),
-                    Text(
-                      "Flutter",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.subtitle2,
-                    )
+                    SizedBox(width: defaultPadding),
+                    Expanded(
+                      child: AnimatedCircularProgressIndicator(
+                        percentage: 0.65,
+                        label: "python",
+                      ),
+                    ),
                   ],
-                )
+                ),
               ],
             ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class AnimatedCircularProgressIndicator extends StatelessWidget {
+  const AnimatedCircularProgressIndicator({
+    super.key,
+    required this.percentage,
+    required this.label,
+  });
+  final double percentage;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 1,
+          child: TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: percentage),
+              duration: defaultDuration,
+              builder: (context, double value, child) => Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      CircularProgressIndicator(
+                        value: value,
+                        color: primaryColor,
+                        backgroundColor: darkColor,
+                      ),
+                      Center(
+                        child: Text(
+                          (value * 100).toInt().toString() + "%",
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                      )
+                    ],
+                  )),
+        ),
+        const SizedBox(
+          height: defaultPadding / 2,
+        ),
+        Text(
+          label,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.subtitle2,
+        )
+      ],
+    );
   }
 }
 
@@ -100,7 +137,7 @@ class AddressInfo extends StatelessWidget {
         children: [
           Text(
             title!,
-            style: TextStyle(color: Colors.white),
+            style: const TextStyle(color: Colors.white),
           ),
           Text(
             text!,
